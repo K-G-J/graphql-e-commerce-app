b807be11835d42cbf1c0fffc0fb72cc6992cdee5
Kate Johnson <johnson.kat.g@gmail.com>
2023-03-02 20:58:15 -0500

    added filter for avgRating

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,12 +1,27 @@
 exports.Query = {
   // name and resolver method
   hello: (parent, args, context) => 'World',
-  products: (parent, { filter }, { products }) => {
+  products: (parent, { filter }, { products, reviews }) => {
     let filteredProducts = products
     if (filter) {
-      if (filter.onSale === true) {
-        filteredProducts = filteredProducts.filter(product => {
+      const { onSale, avgRating } = filter
+      if (onSale) {
+        filteredProducts = filteredProducts.filter((product) => {
           return product.onSale
+        })
+      }
+      if ([1, 2, 3, 4, 5].includes(avgRating)) {
+        filteredProducts = filteredProducts.filter((product) => {
+          let sumRating = 0
+          let numberOfReviews = 0
+          reviews.forEach((review) => {
+            if (review.productId === product.id) {
+              sumRating += review.rating
+              numberOfReviews++
+            }
+          })
+          const avgProductRating = sumRating / numberOfReviews
+          return avgProductRating >= avgRating
         })
       }
     }
